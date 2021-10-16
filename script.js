574685b40a43208c618fbaa3e583c18fea3bed73
archanaknayanar <archana@bigbinary.com>
2021-10-16 17:35:27 +0530

    added random meal fetch

--- script.js
+++ script.js
@@ -56,6 +56,21 @@ function getMealByID(mealID){
     });
 }
 
+//Fetch random meal from API
+function getRandomMeal() {
+    //Clear meals and headings
+    mealsEl.innerHTML = '';
+    resultHeading.innerHTML = '';
+
+    fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+    .then(res => res.json())
+    .then(data => {
+        const meal = data.meals[0];
+
+        addMealToDOM(meal);
+    });    
+}
+
 //Add meal to DOM
 function addMealToDOM(meal) {
     const ingredients = [];
@@ -90,6 +105,8 @@ function addMealToDOM(meal) {
 
 //Event Listner
 submit.addEventListener('submit', searchMeal);
+random.addEventListener('click', getRandomMeal);
+
 mealsEl.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
         if(item.classList) {
