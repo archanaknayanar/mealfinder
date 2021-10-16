e813d092530d7c9ae02367f25eec7f6b2f6957ad
archanaknayanar <archana@bigbinary.com>
2021-10-16 17:07:46 +0530

    first commit

--- script.js
+++ script.js
@@ -58,7 +58,34 @@ function getMealByID(mealID){
 
 //Add meal to DOM
 function addMealToDOM(meal) {
+    const ingredients = [];
     
+    for(let i = 1 ; i <=20 ; i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        } else {
+            break;
+        }
+    }
+
+    singleMealEl.innerHTML = `
+    <div class = "single-meal">
+     <h1>${meal.strMeal}</h2>
+     <img src="${meal.strMealThumb}" alt="${meal.str}" />
+      <div class="single-meal-info">
+      ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+      ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
+      </div>
+      <div class="main">
+       <p1>${meal.strInstructions}</p1>
+       <h2>Ingredients</h2>
+        <ul>
+          ${ingredients.map(ing => `<li>${ing}</li>`).join('')}
+        </ul>
+      </div>
+    </div>
+    `;
+
 }
 
 //Event Listner
